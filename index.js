034042e25cc75f6203841ba93857578dea28df54
Kate Iliu <beviatori@gmail.com>
2024-04-05 09:27:27 -0600

    Change exesise output

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Weekdays = [
   const workout = [
     {
       part: "Upper Body",
-      excercises: [
+      excecises: [
         "Push-Ups",
         "Bench Press",
         "Incline Dumbbell Press",
@@ -24,12 +24,12 @@ const Weekdays = [
     },
     {
       part: "Lower Body",
-      excercises: ["Squats", "Lunges", "Deadlifts", "Leg Press", "Leg Curls", "Standing Calf Raises", "Seated Calf Raises"],
+      excecises: ["Squats", "Lunges", "Deadlifts", "Leg Press", "Leg Curls", "Standing Calf Raises", "Seated Calf Raises"],
   
     },
     {
       part: "Core",
-      excercises: [
+      excecises: [
         "Planks",
         "Abdominal Crunches",
         "Leg Raises",
@@ -39,7 +39,7 @@ const Weekdays = [
     },
     {
       part: "Cardio",
-      excercises: [
+      excecises: [
         "Jogging",
         "Running",
         "Cycling",
@@ -53,9 +53,7 @@ const Weekdays = [
   
 const days = document.getElementById("days");
 const planner = document.getElementById("planner");
-  // function randomWorkout() {
-  //   return workout[Math.floor(Math.random() * workout.length)];
-  // }
+
   
   // Add days on the page
   function weekdays(days) {
@@ -69,29 +67,9 @@ const planner = document.getElementById("planner");
     document.getElementById("days").appendChild(dFrag);
   }
   
-  // function workouts(workout) {
-  //   const workoutDiv = document.getElementById("workout");
-  //   workout.forEach((element) => {
-  //     let optionElement = document.createElement("button");
-  //     optionElement.textContent = element.part;
-  //     optionElement.classList.add("buttonWorkOutPart");
-  //     workoutDiv.appendChild(optionElement);
-  //   });
-  // }
-
 
 
   function fillInTheDayBox(day, workout) {
-    // let selectBodyPart = document.createElement("select");
-    
-    // workout.forEach((element) => {
-    //   console.log(element);
-    //   let optionElement = document.createElement("option");
-    //   optionElement.value = element.part;
-    //   optionElement.textContent = element.part;
-    //   selectBodyPart.appendChild(optionElement);
-    // });
-    // day.appendChild(selectBodyPart);
     const divPart = document.createElement("div");
     divPart.setAttribute('id', 'WorkoutPart')
     day.appendChild(divPart);
@@ -106,6 +84,9 @@ const planner = document.getElementById("planner");
     const divButton = document.createElement("div");
     divButton.setAttribute('id', 'ClearOrRemoveBtn')
     day.appendChild(divButton);
+    let excercisesDiv = document.createElement("div");
+    excercisesDiv.classList.add("excercises")
+    divButton.appendChild(excercisesDiv);
 
     let saveDay = document.createElement("button");
     saveDay.textContent = "Save";
@@ -135,42 +116,66 @@ const planner = document.getElementById("planner");
     fillInTheDayBox(day,workout); 
   }
 
+  function chooseWorkout(workoutName, workout,  exerciseDiv) {
 
-  function chooseWorkout(workoutName, workout) {
-    const checklistDiv = document.createElement("div");
-    checklistDiv.classList.add("workoutChecklist");
     const selectWorkout = workout.find((element) => element.part === workoutName);
-    const workoutPart = document.createElement("p")
-    workoutPart.innerText = selectWorkout.part;
-    checklistDiv.appendChild(workoutPart);
-  
-    console.log(selectWorkout);
-    // Loop through each exercise in the workout
-    const exercise = selectWorkout.excercises;
-    exercise.forEach((exercise) => {
-      console.log(`Inside for in loop for ${exercise}`);
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.value = exercise;
-      checkbox.id = exercise.toLowerCase().replace(/\s+/g, "-"); // Set unique ID for each checkbox
-      checkbox.classList.add("exerciseCheckbox");
-  
-      // Create a label for the checkbox
-      const label = document.createElement("label");
-      label.textContent = exercise;
-      label.setAttribute("for", checkbox.id);
-  
-      // Append checkbox and label to the checklist div
-      checklistDiv.appendChild(checkbox);
-      checklistDiv.appendChild(label);
-    });
-    
-    // Append checklist div to the day div
-    const dayDiv = document.querySelector(".dayInPlanner");
-    const clearOrRemoveBtnDiv = document.getElementById("ClearOrRemoveBtn");
-    dayDiv.insertBefore(checklistDiv, clearOrRemoveBtnDiv);
 
+     // Check if workout was found
+    if (!selectWorkout) {
+      console.error('Workout not found:', workoutName);
+      return; // Exit the function if no workout is found
+    }
+    
+    const workoutPart = document.createElement("p");
+    workoutPart.innerText = selectWorkout.part;
+    exerciseDiv.appendChild(workoutPart);
+
+
+    const exercises = selectWorkout.exercises; 
+    exercises.forEach((exercise) => {
+      let optionElement = document.createElement("button");
+      optionElement.textContent = exercise;
+      optionElement.classList.add("buttonExercise"); 
+      exerciseDiv.appendChild(optionElement);
+    });
   }
+
+
+  // function chooseWorkout(workoutName, workout) {
+  //   const checklistDiv = document.createElement("div");
+  //   checklistDiv.classList.add("workoutChecklist");
+  //   const selectWorkout = workout.find((element) => element.part === workoutName);
+  //   const workoutPart = document.createElement("p")
+  //   workoutPart.innerText = selectWorkout.part;
+  //   checklistDiv.appendChild(workoutPart);
+  
+  //   console.log(selectWorkout);
+  //   // Loop through each exercise in the workout
+  //   const exercise = selectWorkout.excercises;
+  //   exercise.forEach((exercise) => {
+  //     console.log(`Inside for in loop for ${exercise}`);
+  //     const checkbox = document.createElement("INPUT");
+  //     checkbox.type = "checkbox";
+  //     checkbox.value = exercise;
+  //     checkbox.id = exercise.toLowerCase().replace(/\s+/g, "-"); // Set unique ID for each checkbox
+  //     checkbox.classList.add("exerciseCheckbox");
+  
+  //     // Create a label for the checkbox
+  //     const label = document.createElement("label");
+  //     label.textContent = exercise;
+  //     label.setAttribute("for", checkbox.id);
+  
+  //     // Append checkbox and label to the checklist div
+  //     checklistDiv.appendChild(checkbox);
+  //     checklistDiv.appendChild(label);
+  //   });
+    
+  //   // Append checklist div to the day div
+    // const dayDiv = document.querySelector(".dayInPlanner");
+    // const clearOrRemoveBtnDiv = document.getElementById("ClearOrRemoveBtn");
+    // dayDiv.insertBefore(checklistDiv, clearOrRemoveBtnDiv);
+ 
+  // }
 
 
 
@@ -220,21 +225,13 @@ document.getElementById("planner").addEventListener("click", function (event) {
     clickedElement.nodeName === "BUTTON" &&
     clickedElement.closest(".dayInPlanner") && clickedElement.closest(".buttonWorkOutPart")
   ) {
-    
-    console.log("Button inside day div is clicked" + clickedElement.textContent);
-    chooseWorkout(clickedElement.textContent, workout);
+  console.log("Button inside day div is clicked" + clickedElement.textContent);
+  const  exerciseDiv = clickedElement.closest(".excercisesDiv");
+  chooseWorkout(clickedElement.textContent, workout,  exerciseDiv);
   
-
-    // // Disable not chosen buttons
-    // const dayDiv = clickedElement.closest("WorkoutPart");
-    // const buttons = dayDiv.querySelectorAll(".buttonWorkOutPart");
-    // buttons.forEach(button => {
-    //   if (button !== clickedElement) {
-    //     button.disabled = true;
-    //   }
-    // });
   }
 });
+
 
 //Click on Workouts Remove day button inside day div
 document.getElementById("planner").addEventListener("click", function (event) {
@@ -246,12 +243,7 @@ document.getElementById("planner").addEventListener("click", function (event) {
     clickedElement.closest(".dayInPlanner") && clickedElement.closest(".buttonWorkOutRemove")
   ) {
 
-    // const div = document.getElementById("ClearOrRemoveBtn").parentElement;
-    //  div.remove();
-    //  const weekDaysButton = days.querySelectorAll(".buttonDay");
-    //  weekDaysButton.forEach(element => {
-    //      element.classList.remove("choosenDay");
-    //  });
+
     const dayDiv = clickedElement.closest(".dayInPlanner");
         dayDiv.remove();
 
